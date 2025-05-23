321475830f7ead864b57b231001a802c396a8cad
Mr_Trans <x843824811@163.com>
2025-05-23 17:49:23 +0800

    learn map and slice string

--- easygolang/ch2/first_test.go
+++ easygolang/ch2/first_test.go
@@ -1,6 +1,7 @@
 package try_test
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -47,4 +48,74 @@ func TestSwitch(t *testing.T){
 	// switch os:=runtime.GOOS; os{
 	// 	case "d"
 	// }
+}
+
+func TestArrayInit(t *testing.T){
+	var arr [3]int
+	arr1 := [4]int{1,2,3,4}
+	arr3 := [...]int{1,2,3,4,5}
+	arr1[1] = 5
+	t.Log(arr[1],arr[2])
+	t.Log(arr1,arr3)
+}
+
+
+func TestArrayTravel(t *testing.T){
+	arr3:=[...]int{1,3,4,5}
+	for i:=0;i<len(arr3);i++{
+		t.Log(arr3[i])
+	}
+	arr := arr3[:1]
+	t.Log(arr)
+}
+
+
+func TestSliceTravel(t *testing.T){
+	var s0 []int
+	t.Log(len(s0),cap(s0))
+	s0 = append(s0, 1)
+	t.Log(len(s0),cap(s0))
+
+	s1 := []int{1,2,3,4}
+	t.Log(len(s1),cap(s1))
+
+	s2 := make([]int,3,5)
+	t.Log(len(s2),cap(s2))
+}
+
+
+func TestSliceGrowing(t *testing.T){
+	s := []int{}
+	for i := 0;i<10;i++{
+		s = append(s, i)
+		t.Log(s,len(s),cap(s))
+	}
+}
+
+func TestSliceShare(t *testing.T){
+	nums := []int{0,1,2,3,4,5,56,6,7}
+	s1 := nums[3:5]
+	s2 := nums[4:6]
+	t.Log(len(s1),cap(s1),s1[0])
+	t.Log(len(s2),cap(s2))
+	alph := make([]string,3,5)
+	t.Log(alph[0])
+}
+
+
+func TestInitMap(t *testing.T){
+	m1 := map[int]int{1:2,2:3,3:6}
+	t.Log(m1[2])
+	for k,v := range m1{
+		t.Log(k,v)
+	}
+}
+
+func TestStringFn(t *testing.T){
+	s:="A,B,C"
+	parts := strings.Split(s,",")
+	for _,part := range parts{
+		t.Log(part)
+	}
+	t.Log(strings.Join(parts,"_"))
 }
